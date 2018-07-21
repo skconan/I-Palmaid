547335f5def7be4c9859e9d69683d0b4834b15c7
Pornpavee Seri-umnuoy <pornpavee.se@ku.th>
2018-07-21 19:09:13 +0700

    animation add

--- software/javascirpt/front.js
+++ software/javascirpt/front.js
@@ -1,3 +1,6 @@
+var pos = 370;
+var stop = false;
+
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
@@ -13,11 +16,12 @@ function myFunction() {
 
 function myMove() {
     var elem = document.getElementById("myAnimation");   
-    var pos = 350;
-    var id = setInterval(frame, 10);
+    var id = setInterval(frame, 1);
     function frame() {
-      if (pos == 1050) {
+      console.log(stop)
+      if (pos == 1050 || stop == true) {
         clearInterval(id);
+        stop = false
       } else {
         pos++; 
        
@@ -25,3 +29,29 @@ function myMove() {
       }
     }
   }
+
+
+  function myBack() {
+    var elem = document.getElementById("myAnimation");   
+    var id = setInterval(frame, 1);
+    function frame() {
+      console.log(stop)
+      if (pos == 370 || stop == true) {
+        clearInterval(id);
+        stop = false
+      } else {
+        pos--; 
+       
+        elem.style.left = pos + 'px'; 
+      }
+    }
+  }
+
+
+
+function stopRobot() {
+  stop = true;
+}
+
+
+
