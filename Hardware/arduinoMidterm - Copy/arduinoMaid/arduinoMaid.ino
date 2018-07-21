db278d406cf7bbe832558513db1efda4e9bc602f
thtiiz <napon-na123@hotmail.com>
2018-07-21 12:04:40 +0700

    add 3 sensors

---
+++ Hardware/arduinoMidterm - Copy/arduinoMaid/arduinoMaid.ino
@@ -0,0 +1,214 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+#include "DHT.h"
+#define SW 2
+#define BUZZER 3
+#define LED_Y 4
+#define servo 7
+#define DHTPIN A1
+#define DHTTYPE DHT22
+#define ldr A0
+#define trigger_pin 5
+#define in_pin 6
+
+Servo myservo;
+SoftwareSerial se_read(12, 13); // write only
+SoftwareSerial se_write(10, 11); // read only
+
+long duration, cm;
+
+long microsecondsToCentimeters( long microseconds)
+{
+  return microseconds / 29 / 2;
+}
+
+
+///////////////////////////////
+///////////////////////////////
+struct ProjectData {
+  int32_t sw;
+  int32_t light;
+  int32_t motor;
+  int32_t ultrasonic;
+} project_data = { 0, 0, 0 , 0};
+
+struct ServerData {
+  int32_t switchStatus;
+  int32_t motorStatus;
+  int32_t goRoom;
+} server_data = { 0, 0, 0};
+
+const char GET_SERVER_DATA = 1;
+const char GET_SERVER_DATA_RESULT = 2;
+const char UPDATE_PROJECT_DATA = 3;
+
+void ringOn(){
+  tone(BUZZER, 2000);
+  delay(500);
+  noTone(BUZZER);
+  delay(500);
+  tone(BUZZER, 2000);
+  delay(200);
+  noTone(BUZZER);
+  delay(200);
+  tone(BUZZER, 2000);
+  delay(200);
+  noTone(BUZZER);
+  delay(200);
+  tone(BUZZER, 2000);
+  delay(200);
+  noTone(BUZZER);
+}
+
+void send_to_nodemcu(char code, void *data, char data_size) {
+  char *b = (char*)data; 
+  char sent_size = 0;
+  while (se_write.write(code) == 0) {
+    delay(1);
+    
+  }
+  while (sent_size < data_size) {
+    sent_size += se_write.write(b, data_size);
+    delay(1);
+  }
+}
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(SW, INPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(LED_Y, OUTPUT);
+  pinMode(ldr, INPUT);
+  pinMode(trigger_pin, OUTPUT);
+  pinMode(in_pin, INPUT);
+  myservo.attach(servo);
+  myservo.write(0);
+  dht.begin();
+  
+  Serial.begin(115200);
+  se_read.begin(38400);
+  se_write.begin(38400);
+  pinMode(LED_BUILTIN, OUTPUT);
+  while (!se_read.isListening()) {
+    se_read.listen();
+  }
+  //pinMode(SW,INPUT_PULLUP);
+  Serial.println((int)sizeof(ServerData));
+  Serial.println("ARDUINO READY!");
+}
+
+uint32_t last_sent_time = 0;
+boolean is_data_header = false;
+char expected_data_size = 0;
+char cur_data_header = 0;
+char buffer[256];
+int8_t cur_buffer_length = -1;
+int32_t b = -1;
+
+void loop() {
+//  Serial.println("LOOP");
+  delay(500);
+  uint32_t cur_time = millis();
+  //read from sensor....
+  Serial.println("\n############## Project Data ################");
+  int a = digitalRead(SW); /////////SWITCH
+  ///////////////////////////////////////// Ultrasonic
+  digitalWrite(trigger_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger_pin, LOW);
+  duration = pulseIn(in_pin, HIGH);
+  cm = microsecondsToCentimeters(duration);
+  Serial.print("Ultrasonic: ");
+  Serial.print(cm);
+  Serial.println(" cm.");
+  ////////////////////////////////////////
+  project_data.sw = a; //set sw to project data
+  Serial.print("SW Send to Node: ");
+  Serial.println(project_data.sw);
+  project_data.light = analogRead(ldr); ////Light
+  Serial.print("light :");
+  Serial.println(project_data.light);
+//  project
+  if(a == 0){
+    Serial.println("Press");
+    delay(500);
+  }
+  Serial.println("###################################\n");
+  delay(1000);
+  //Serial.println(server_data.airOn);
+  /*project_data.plus = b;
+  if(server_data.plus == 1){
+    digitalWrite(LED,HIGH);
+  }
+  else{
+    digitalWrite(LED,LOW);
+  }*/
+//  Serial.print("SizeS: ");
+//  Serial.println(sizeof(ServerData));
+//  Serial.print("SizeP: ");
+//  Serial.println(sizeof(ProjectData));
+  if (cur_time - last_sent_time > 500) {//always update
+//    Serial.println(project_data.plus);
+    send_to_nodemcu(GET_SERVER_DATA, &server_data, sizeof(ServerData));
+    send_to_nodemcu(UPDATE_PROJECT_DATA, &project_data, sizeof(ProjectData));
+    Serial.println("SEND");
+    last_sent_time = cur_time;
+    
+  }
+  //send to nodemcu
+//  Serial.println("Start Reading");
+  //read data from server pass by nodemcu
+  while (se_read.available()) {
+    char ch = se_read.read();
+//    Serial.println("Reading success!!!!!!!!!!!");
+    //Serial.println("Serial read");
+//    Serial.print("RECV: ");
+//    Serial.println((byte)ch);
+    if (cur_buffer_length == -1) {
+      cur_data_header = ch;
+      switch (cur_data_header) {
+        case GET_SERVER_DATA_RESULT:
+        //unknown header
+          expected_data_size = sizeof(ServerData);
+          cur_buffer_length = 0;
+          break;
+      }
+    } else if (cur_buffer_length < expected_data_size) {
+      buffer[cur_buffer_length++] = ch;
+      if (cur_buffer_length == expected_data_size) {
+        switch (cur_data_header) {
+          case GET_SERVER_DATA_RESULT: {
+            ServerData *data = (ServerData*)buffer;
+            //use data to control sensor
+            Serial.print("SW from Node: ");
+            Serial.println(data->switchStatus);
+            //Serial.println(server_data.switchStatus);
+            
+            if(data -> switchStatus == 0) {
+              digitalWrite(LED_Y, HIGH);
+            }
+            else if(data -> switchStatus != 0) {
+              digitalWrite(LED_Y, LOW);
+            }
+
+            if(project_data.light < 200) {
+              //STOP MOTOR
+            }
+
+            if(project_data.ultrasonic < 10) {
+              //STOP MOTOR
+            }
+            //server_data.plus = data->plus;
+            Serial.println("UPDATE");
+          } break;
+        }
+        cur_buffer_length = -1;
+      }
+    }
+  }
+}
+
